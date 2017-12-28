3df40d7b456144a91a0c4f2d397a66c9086321f6
Thomas 'Whytewolf' Phipps <github@bad4.us>
2017-12-28 11:51:02 -0800

    nuke openstack_* as I just dont have the time

--- _engines/genesis_dhcp.py
+++ _engines/genesis_dhcp.py
@@ -11,15 +11,19 @@ import salt.utils.event
 
 log = logging.getLogger(__name__)
 
-
-
-
-
-
 def start():
     '''
     Setup dnsmasq for pxe boot
     '''
-    if __opts__['__role'] !== 'master':
+    if __opts__['__role'] == 'master':
+        event_bus = salt.utils.event.get_master_event(
+                __opts__,
+                __opts__['sock_dir'],
+                listen=True)
         while True:
-            log.error('genesis_dhcp: this is only meant to run on master')
+            event = event_bus.get_event()
+            jevent = json.dumps(event)
+            if event:
+                log.debug(jevent)
+    else:
+        log.error('genesis_dhcp: this is only meant to run on master')
